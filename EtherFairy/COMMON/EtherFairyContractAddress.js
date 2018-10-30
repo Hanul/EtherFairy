51bd2cac7d24d72493c4717cbac1cec2e08d4d6d
Hanul <hanul@hanul.me>
2018-10-30 21:05:13 +0900

    no message

--- EtherFairy/COMMON/EtherFairyContractAddress.js
+++
@@ -1,8 +0,0 @@
-EtherFairy.EtherFairyContractAddress = CONFIG.isDevMode !== true ?
-
-// 운영 모드
-'' :
-
-// 개발 모드
-// Kovan
-'0xf5029ec885491641b225e2c84aaaabd185a53888';
--- EtherFairy/COMMON/FairyMarketContractAddress.js
+++
@@ -1,8 +0,0 @@
-EtherFairy.FairyMarketContractAddress = CONFIG.isDevMode !== true ?
-
-// 운영 모드
-'' :
-
-// 개발 모드
-// Kovan
-'0x52a08be93b102222a9b130fe82aab340a728688d';
